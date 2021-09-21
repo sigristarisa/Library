a0a566603e482c878f37ac65253ac92179d626ac
sigristarisa <arisasigrist.ch@gmail.com>
2021-09-21 17:07:43 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Assign a book //
 class Book {
-  constructor(title, author, pages, isRead) {
+  constructor(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
   }
 }
 
@@ -22,13 +21,27 @@ class Library {
     return this.library;
   };
 
+  // Remove a book in your library //
+  removeBook = (title) => {
+    this.library.filter((book) => {
+      book.title !== title;
+    });
+  };
+
   // Display your book in your library
   showLibrary = () => {
     this.library.forEach((book, index) => {
+      // create a book "card" //
       const booklist = document.getElementById("booklist");
       const bookContainer = document.createElement("div");
       bookContainer.setAttribute("id", index);
 
+      // add the "remove" button //
+      const removeButton = document.createElement("button");
+      removeButton.setAttribute("class", "removeButton");
+      removeButton.textContent = "Remove";
+
+      // add the details of the book //
       const bookTitle = document.createElement("h3");
       const bookAuthor = document.createElement("p");
       const bookPages = document.createElement("p");
@@ -37,6 +50,8 @@ class Library {
       bookContainer.appendChild(bookTitle);
       bookContainer.appendChild(bookAuthor);
       bookContainer.appendChild(bookPages);
+      bookContainer.appendChild(removeButton);
+
       bookTitle.innerText = book.title;
       bookAuthor.innerText = "author: " + book.author;
       bookPages.innerText = "pages: " + book.pages + " pages";
@@ -55,15 +70,15 @@ const openBookForm = () => {
 };
 
 // console.log
-const normalPeople = new Book("Normal People", "Sally Rooney", 345, true);
-const aLittleLife = new Book("A Little Life", "Hanya Yanagihara", 734, true);
+const normalPeople = new Book("Normal People", "Sally Rooney", 345);
+const aLittleLife = new Book("A Little Life", "Hanya Yanagihara", 734);
 const catcherInTheRye = new Book(
   "Catcher in the Rye",
   "J.D. Salinger",
   300,
   true
 );
-const openWater = new Book("Open Water", "Caleb Azumah Nelson", 200, true);
+const openWater = new Book("Open Water", "Caleb Azumah Nelson", 200);
 
 const library = new Library();
 
@@ -73,3 +88,4 @@ library.addBookToLibrary(catcherInTheRye);
 library.addBookToLibrary(openWater);
 
 library.showLibrary();
+console.log(library.library);
