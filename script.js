3fc0e6f5d619bfd03ea043e25514f92a7fc29e01
sigristarisa <arisasigrist.ch@gmail.com>
2021-09-22 12:09:39 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assign a book //
 class Book {
   constructor(title, author, pages) {
     this.title = title;
@@ -7,39 +6,23 @@ class Book {
   }
 }
 
-// Create a library //
 class Library {
   constructor() {
     this.library = [];
   }
 
-  // Add a new book in your library //
-  addBookToLibrary = (newBook) => {
-    if (!this.library.includes(newBook)) {
-      this.library.push(newBook);
-    }
-    return this.library;
-  };
-
-  // Remove a book in your library //
-  removeBook = (title) => {
-    this.library.filter((book) => {
-      book.title !== title;
-    });
-  };
-
-  // Display your book in your library
-  showLibrary = () => {
-    this.library.forEach((book, index) => {
-      // create a book "card" //
+  // Create a book card
+  bookCard = () => {
+    this.library.forEach((book) => {
+      // create an container for the book card //
       const booklist = document.getElementById("booklist");
       const bookContainer = document.createElement("div");
       bookContainer.setAttribute("id", index);
 
       // add the "remove" button //
-      const removeButton = document.createElement("button");
-      removeButton.setAttribute("class", "removeButton");
-      removeButton.textContent = "Remove";
+      // const removeButton = document.createElement("button");
+      // removeButton.setAttribute("class", "removeButton");
+      // removeButton.textContent = "Remove";
 
       // add the details of the book //
       const bookTitle = document.createElement("h3");
@@ -57,10 +40,22 @@ class Library {
       bookPages.innerText = "pages: " + book.pages + " pages";
     });
   };
+
+  // Add the book from the form into the library //
+  createBook = () => {
+    const bookForm = document.getElementById("bookForm");
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+
+    this.library.push(new Book(title, author, pages));
+    console.log(this.library);
+    return bookForm.reset();
+  };
 }
 
 // Display the new book form once the button is clicked //
-const openBookForm = () => {
+showBookForm = () => {
   const bookForm = document.getElementById("bookForm");
   if (bookForm.style.display === "none") {
     bookForm.style.display = "block";
@@ -69,23 +64,4 @@ const openBookForm = () => {
   }
 };
 
-// console.log
-const normalPeople = new Book("Normal People", "Sally Rooney", 345);
-const aLittleLife = new Book("A Little Life", "Hanya Yanagihara", 734);
-const catcherInTheRye = new Book(
-  "Catcher in the Rye",
-  "J.D. Salinger",
-  300,
-  true
-);
-const openWater = new Book("Open Water", "Caleb Azumah Nelson", 200);
-
 const library = new Library();
-
-library.addBookToLibrary(normalPeople);
-library.addBookToLibrary(aLittleLife);
-library.addBookToLibrary(catcherInTheRye);
-library.addBookToLibrary(openWater);
-
-library.showLibrary();
-console.log(library.library);
